8dca4a9c5002da1b25ccf487b220de2179c645f1
App Academy Student <student@appacademy.io>
2015-06-12 16:43:40 -0700

    Create Rent Form

--- app/assets/javascripts/views/bikes/bike_show.js
+++ app/assets/javascripts/views/bikes/bike_show.js
@@ -14,7 +14,74 @@ window.TakeASpin.Views.BikeShow = Backbone.View.extend({
     });
 
     this.$el.html(content);
+    this.formatDates();
 
     return this;
+  },
+
+  events: {
+    'dp.hide .time': 'blur',
+    'dp.hide .time, .date': 'saveDates',
+    'dp.change #pickup-date': 'modReturnDate',
+    'dp.change #return-date': 'modPickupDate',
+    'click .rent-button': 'submitRentalForm'
+  },
+
+  submitRentalForm: function() {
+    alert('rent!');
+  },
+
+  modReturnDate: function(event) {
+    var date = event.date;
+    if (date); {
+      this.$('#return-date').data("DateTimePicker").minDate(date);
+    }
+  },
+
+  saveDates: function(event) {
+    this.pickupDate = $('#pickup-date').val();
+    this.pickupTime = $('#pickup-time').val();
+    this.returnDate = $('#return-date').val();
+    this.returnTime = $('#return-time').val();
+    // console.log(this.pickupDate, this.returnDate, this.pickupTime, this.returnTime);
+    // if (this.pickupDate && this.returnDate && this.pickupTime && this.returnTime) {
+    //   this.fetchBikes(event)
+    // }
+  },
+
+  modPickupDate: function(event) {
+    var date = event.date;
+    if (date); {
+      this.$('#pickup-date').data("DateTimePicker").maxDate(date);
+    }
+  },
+
+  blur: function(event) {
+    $(event.currentTarget).blur();
+  },
+
+  formatDates: function() {
+    ['#pickup-date', '#return-date'].forEach(function(input) {
+      this.$(input).datetimepicker({
+        format: 'MMM-DD',
+        minDate: moment(),
+        maxDate: moment().add(3, 'months'),
+        showClose: true,
+        showClear: true,
+        showTodayButton: true
+      });
+    }.bind(this));
+
+    ['#pickup-time', '#return-time'].forEach(function(input) {
+      this.$(input).datetimepicker({
+        format: 'H:mm A',
+        minDate: moment(),
+        maxDate: moment().add(3, 'months'),
+        toolbarPlacement: 'bottom',
+        showClose: true,
+        showClear: true,
+        showTodayButton: true
+      });
+    }.bind(this));
   }
 });
